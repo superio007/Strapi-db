689d28702928f532028a21f8ad1b7d27f9b3ce8c
Kiran Dhoke <dhokekiran98@gmail.com>
2025-03-19 12:59:17 +0530

    Update PostgreSQL dependency and add production environment configuration for Strapi

--- Tasa-Strapi/config/database.ts
+++ Tasa-Strapi/config/database.ts
@@ -25,7 +25,7 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', '172.18.0.3'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
